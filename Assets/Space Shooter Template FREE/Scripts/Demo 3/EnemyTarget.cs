d2dc452a251ec95bb0794c032de8da6f2939afe4
BNgoc005 <nguyenngoc081005@gmail.com>
2026-02-05 20:41:33 +0700

    DEMOE part 4

--- Assets/Space Shooter Template FREE/Scripts/Demo 3/EnemyTarget.cs
+++ Assets/Space Shooter Template FREE/Scripts/Demo 3/EnemyTarget.cs
@@ -5,16 +5,16 @@ public class EnemyTarget : MonoBehaviour
     public GameObject explosionPrefab; // Ô để kéo hiệu ứng nổ vào
 
     // Hàm này chạy khi có cái gì đó đâm vào kẻ địch
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         // Kiểm tra nếu cái đâm vào có Tag là "Bullet"
-        if (other.CompareTag("Bullet"))
+        if (collision.CompareTag("Bullet"))
         {
             // Tạo vụ nổ tại vị trí kẻ địch
             Instantiate(explosionPrefab, transform.position, transform.rotation);
             
             // Xóa viên đạn
-            Destroy(other.gameObject);
+            Destroy(collision.gameObject);
             
             // Xóa kẻ địch
             Destroy(gameObject);
--- Assets/Space Shooter Template FREE/Scripts/Player_b_m/PlayerShootting.cs
+++ Assets/Space Shooter Template FREE/Scripts/Player_b_m/PlayerShootting.cs
@@ -2,24 +2,25 @@ using UnityEngine;
 
 public class PlayerShootting : MonoBehaviour
 {
-public GameObject bulletPrefabs;
-public float shootingInterval;
-private float lastBulletTime;
-void Update()
-{
-if (Input.GetMouseButton(0))
-{
-if (Time.time - lastBulletTime >
-shootingInterval)
-{
-ShootBullet();
-lastBulletTime = Time.time;
-}
-}
-}
+    public GameObject bulletPrefabs;
+    public float shootingInterval;
+    private float lastBulletTime;
+    public Vector3 bulletOffset;
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (Time.time - lastBulletTime >
+            shootingInterval)
+            {
+                ShootBullet();
+                lastBulletTime = Time.time;
+            }
+        }
+    }
 
-private void ShootBullet()
-{
-Instantiate(bulletPrefabs, transform.position, transform.rotation);
-}
+    private void ShootBullet()
+    {
+        Instantiate(bulletPrefabs, transform.position + bulletOffset, transform.rotation);
+    }
 }
