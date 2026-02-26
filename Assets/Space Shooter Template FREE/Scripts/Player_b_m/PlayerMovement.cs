43f121f2480e2bab95f3a2c661ca592038ad81c9
BNgoc005 <nguyenngoc081005@gmail.com>
2026-02-26 11:27:02 +0700

    Hoan thanh Part 5: Sound va UI Game Over

--- Assets/Space Shooter Template FREE/Scripts/Player_b_m/PlayerMovement.cs
+++ Assets/Space Shooter Template FREE/Scripts/Player_b_m/PlayerMovement.cs
@@ -2,18 +2,43 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // 1. Khai báo biến này để kéo cái Canvas vào
+    public GameObject gameOverCanvas;
+
     void Start()
     {
-        
+        // Lúc đầu game thì ẩn màn hình Game Over đi cho chắc
+        if (gameOverCanvas != null) {
+            gameOverCanvas.SetActive(false);
+        }
     }
 
     void Update()
     {
-        Debug.Log(Input.mousePosition);
-        var worldPoint =
-        Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Điều khiển máy bay đi theo chuột
+        var worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         worldPoint.z = 0;
         transform.position = worldPoint;
+    }
+
+    // 2. Hàm này để gọi hiện chữ Game Over khi thua
+    public void ShowGameOver() 
+    {
+        if (gameOverCanvas != null) 
+        {
+            gameOverCanvas.SetActive(true); 
+        }
+    }
+
+    // 3. Tặng thêm cho Ngọc: Hàm tự động hiện chữ khi va chạm
+    // (Giả sử thiên thạch hoặc địch có Tag là "Enemy")
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            ShowGameOver();
+            // Có thể thêm hiệu ứng nổ hoặc ẩn máy bay ở đây
+            // gameObject.SetActive(false); 
+        }
     }
 }
